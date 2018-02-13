034a1db2c0fc443b300928c39ae4c1a250b7214d
Utente <Utente@UTENTE-PC>
2018-02-13 17:39:57 +0100

    aggiunti nuovi test

--- SolutionCalcolatrice/LibraryCalc.Tests/Calc.Test.cs
+++ SolutionCalcolatrice/LibraryCalc.Tests/Calc.Test.cs
@@ -58,7 +58,8 @@ namespace LibraryCalc.Tests
         // public void MyTestCleanup() { }
         //
         #endregion
-         private static double Somma (double num1,double num2)
+        [TestMethod]
+        public static double Somma (double num1,double num2)
         {
             throw new NotImplementedException();
         }
@@ -106,7 +107,8 @@ namespace LibraryCalc.Tests
 
             Assert.AreEqual(ris, somma);
         }
-        private static double Divisione(double num1, double num2)
+        [TestMethod]
+        public static double Divisione(double num1, double num2)
         {
             throw new NotImplementedException();
         }
@@ -154,5 +156,192 @@ namespace LibraryCalc.Tests
 
             Assert.AreEqual(ris, Divisione);
         }
+        [TestMethod]
+        public static double Massimo(double num1, double num2)
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void TestMassimo()
+        {
+            double num1 = 5;
+            double num2 = 2;
+            double max = 5;
+
+            double Massimo = Calc.Massimo(num1, num2);
+            Assert.AreEqual(max, Massimo);
+        }
+        [TestMethod]
+        public void TestMassimoPer3e4()
+        {
+            double num1 = 3;
+            double num2 = 4;
+            double max = 4;
+
+            double Massimo = Calc.Massimo(num1, num2);
+            Assert.AreEqual(max, Massimo);
+        }
+        public void TestMassimoPerDueSette()
+        {
+            double num1 = 7;
+            double num2 = 7;
+            double max = 7;
+
+            double Massimo = Calc.Massimo(num1, num2);
+            Assert.AreEqual(max, Massimo);
+        }
+        [TestMethod]
+        public static double Minimo(double num1, double num2)
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void TestMinimo()
+        {
+            double num1 = 5;
+            double num2 = 2;
+            double min = 5;
+
+            double Minimo = Calc.Minimo(num1, num2);
+            Assert.AreEqual(min, Minimo);
+        }
+        [TestMethod]
+        public void TestMinimoPer3e4()
+        {
+            double num1 = 3;
+            double num2 = 4;
+            double min = 4;
+
+            double Minimo = Calc.Minimo(num1, num2);
+            Assert.AreEqual(min, Minimo);
+        }
+        [TestMethod]
+        public void TestMinimoPerDueSette()
+        {
+            double num1 = 7;
+            double num2 = 7;
+            double min = 7;
+
+            double Minimo = Calc.Minimo(num1, num2);
+            Assert.AreEqual(min, Minimo);
+        }
+        [TestMethod]
+        public static double Media(double num1, double num2)
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void TestMedia()
+        {
+            double num1 = 5;
+            double num2 = 2;
+            double ris = 3.5;
+
+            double Media = Calc.Media(num1, num2);
+            Assert.AreEqual(ris, Media);
+        }
+        [TestMethod]
+        public void TestMediaPer3e4()
+        {
+            double num1 = 3;
+            double num2 = 4;
+            double ris = 3.5;
+
+            double Media = Calc.Media(num1, num2);
+            Assert.AreEqual(ris, Media);
+        }
+        [TestMethod]
+        public void TestMediaPerDueSette()
+        {
+            double num1 = 7;
+            double num2 = 7;
+            double ris = 7;
+
+            double Media = Calc.Media(num1, num2);
+            Assert.AreEqual(ris, Media);
+        }
+        [TestMethod]
+        public static double MassimoTre(double num1, double num2,double num3)
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void TestMassimoTraTreNumeri()
+        {
+            double num1 = 7;
+            double num2 = 6;
+            double num3 = 5;
+            double max = 7;
+
+            double massimo = MassimoTre(num1, num2,num3);
+            Assert.AreEqual(max, massimo);
+        }
+        [TestMethod]
+        public void TestMassimoTraUnoQuattroSei()
+        {
+            double num1 = 1;
+            double num2 = 4;
+            double num3 = 6;
+            double max = 6;
+
+            double massimo = Calc.MassimoTre(num1, num2, num3);
+            Assert.AreEqual(max, massimo);
+        }
+        [TestMethod]
+        public static double MinimoTre(double num1, double num2, double num3)
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void TestMinimoTraTreNumeri()
+        {
+            double num1 = 7;
+            double num2 = 6;
+            double num3 = 5;
+            double min = 5;
+
+            double minimo = MinimoTre(num1, num2, num3);
+            Assert.AreEqual(min, minimo);
+        }
+        [TestMethod]
+        public void TestMinimoTraUnoQuattroSei()
+        {
+            double num1 = 1;
+            double num2 = 4;
+            double num3 = 6;
+            double min = 1;
+
+            double minimo = Calc.MinimoTre(num1, num2, num3);
+            Assert.AreEqual(min, minimo);
+        }
+        [TestMethod]
+        public static double Media3(double num1, double num2, double num3)
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void TestMediaTraTreNumeri()
+        {
+            double num1 = 7;
+            double num2 = 6;
+            double num3 = 5;
+            double ris = 5;
+
+            double media = Calc.Media3(num1, num2, num3);
+            Assert.AreEqual(ris, media);
+        }
+
+        [TestMethod]
+        public void TestMediaTraUnoQuattroSei()
+        {
+            double num1 = 1;
+            double num2 = 4;
+            double num3 = 6;
+            double ris = 1;
+
+            double media = Calc.Media3(num1, num2, num3);
+            Assert.AreEqual(ris, media);
+        }
+
     }
 }
